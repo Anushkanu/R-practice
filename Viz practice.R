6312bc9c05813442a7c42852e009af0913b6662a
Anushka <115009084+Anushkanu@users.noreply.github.com>
2026-02-22 13:41:57 +0530

    Add files via upload

--- Viz practice.R
+++ Viz practice.R
@@ -11,13 +11,15 @@ data(package = "modeldata")
 data("crickets", package = "modeldata")
 View(crickets)
 
-#the basic: 
-#scatterplot
+#the basic:
+
+#scatter plot
 ggplot(crickets, aes(x=temp,
                      y=rate,
                      color=species)) + 
 #the type of viz needed
   geom_point() +
+  
   labs(x="Temperature",
        y="Chrip Rate",
        color = "Species:",
@@ -37,29 +39,74 @@ ggplot(crickets, aes(x=temp,
        title="Cricket Chirps",
        caption="Mcd 2009")
 
-#learn more abt options for ggplot
-#with ?ggpoiny
+#learn more about options for ggplot
+#with ?gg_point
 #adding another layer
 ggplot(crickets, aes(x=temp,
                      y=rate)) +
   geom_point() +
-#regression line best fit line
   geom_smooth(method = "lm",
               se = FALSE)+
   labs(x="Temperature",
        y="Chrip Rate",
        title="Cricket Chirps",
        caption="Mcd 2009")
+
+#with colors
 ggplot(crickets, aes(x=temp,
                      y=rate,
                      color=species)) + 
-  #the type of viz needed
   geom_point() +
   geom_smooth(method = "lm",
-              se=FALSE)+
+            se=FALSE)+
   labs(x="Temperature",
        y="Chrip Rate",
        color = "Species:",
        title="Cricket Chirps",
        caption="Mcd 2009")+
   scale_color_brewer(palette = "Dark2")
+
+#other plots
+ggplot(crickets, aes(x=rate))+
+  geom_histogram(bins=15) #one quantitative variable
+
+ggplot(crickets, aes(x=rate))+
+  geom_freqpoly(bins=15)
+
+ggplot(crickets, aes(x=species)) +
+  geom_bar(color="pink",
+           fill="lightblue")
+
+ggplot(crickets, aes(x=species, 
+                     fill=species))+
+  geom_bar()+
+  scale_fill_brewer(palette = "Dark2")
+
+ggplot(crickets, aes(x=species,
+                     y=rate,
+                     color=species))+
+  geom_boxplot(show.legend = FALSE) +
+  scale_color_brewer(palette="Dark2")+
+  theme_classic()
+
+#faceting
+
+#not great way:
+ggplot(crickets, aes(x=rate,
+                     fill=species))+
+  geom_histogram()+
+  scale_fill_brewer(palette = "Dark2")
+
+ggplot(crickets, aes(x=rate,
+                     fill=species))+
+  geom_histogram(bins=15)+
+  facet_wrap(~species)+
+  scale_fill_brewer(palette = "Dark2")
+?facet_wrap
+
+ggplot(crickets, aes(x=rate,
+                     fill=species))+
+  geom_histogram(bins=15)+
+  facet_wrap(~species,
+             ncol=1)+
+  scale_fill_brewer(palette = "Dark2")
